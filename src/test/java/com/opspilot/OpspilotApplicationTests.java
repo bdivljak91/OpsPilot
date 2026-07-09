51cf05a25d4e1b61931c49aa52e19ee7c21352eb
Nikolic Biljana (uib98987) <biljana.nikolic@continental-corporation.com>
2026-07-09 12:36:49 +0200

    feat: initialize Spring Boot project

---
+++ src/test/java/com/opspilot/OpspilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opspilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpspilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
